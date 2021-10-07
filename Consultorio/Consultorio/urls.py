b902db186fb579927db63d50c9577f4d36adf0be
LuisaOPenagos <lu98fernanda@gmail.com>
2021-10-07 10:25:30 -0500

    confirmar

--- Consultorio/Consultorio/urls.py
+++ Consultorio/Consultorio/urls.py
@@ -4,9 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('registros/', include('registros.urls')),
-<<<<<<< HEAD
     path('iniciosesion/', include('iniciosesion.urls')),
-=======
     path('salaespera/', include('salaespera.urls')),
->>>>>>> 2dbd47e6c5b5bd5de2b1939ae4b5ea170eb6fcc8
 ]
