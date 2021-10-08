6958ede82f45ce1b278a978e4925800a06ac617b
Xiomy311 <edxirume@hotmail.com>
2021-10-07 23:50:50 -0500

    Update

--- Consultorio/menucita/views.py
+++ Consultorio/menucita/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def menucita(request):
-    return HttpResponse("Menu 1")
+    parametros=""
+    return render(request, 'menucita.html')
  
